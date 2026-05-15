f612a7f48407c945764ba27dd81dd5f9606382dd
adalyyy <fatimaadalys@gmail.com>
2026-05-15 12:57:55 -0600

    Solo se creo el controlador, se hicieron todas las rutas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\LibroController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/libros', [LibroController::class, 'index']);
+Route::post('/libros', [LibroController::class, 'store']);
+Route::get('/libros/{id}', [LibroController::class, 'show']);
+Route::put('/libros/{id}', [LibroController::class, 'update']);
+Route::delete('/libros/{id}', [LibroController::class, 'destroy']);
